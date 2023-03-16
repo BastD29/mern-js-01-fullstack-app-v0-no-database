fc2cc9c5773cebfa38a18a91001e305b518aabfe
Bastien Daniel <bastien.daniel@rawfish.com>
2023-03-16 15:59:38 +0100

    second commit

--- server/routes/tea.js
+++ server/routes/tea.js
@@ -1,10 +1,14 @@
 import express from "express";
 import teaController from "../controllers/tea.js";
 
-// heyyyyyyyyyyyyyyy
-
 const router = express.Router();
 
+router.get("/tea", teaController.getAllTea);
 router.post("/tea", teaController.newTea);
+router.delete("/tea", teaController.deleteAllTea);
+
+router.get("/tea/:name", teaController.getOneTea);
+router.post("/tea/:name", teaController.newComment);
+router.delete("/tea/:name", teaController.deleteOneTea);
 
 export default router;
