ac428aecc1b303a98e4fc123912e6a3d84044bf7
Bastien Daniel <bastien.daniel@rawfish.com>
2023-03-16 15:48:40 +0100

    second commit

--- server/routes/tea.js
+++ server/routes/tea.js
@@ -1,6 +1,8 @@
 import express from "express";
 import teaController from "../controllers/tea.js";
 
+// heyyyyyyyyyyyyyyy
+
 const router = express.Router();
 
 router.post("/tea", teaController.newTea);
